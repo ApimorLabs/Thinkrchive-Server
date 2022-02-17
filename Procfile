0e819193c37710ff96c0f378f7f765f2a64dd2e7
racka98 <bmkilaha@gmail.com>
2022-02-17 16:26:22 +0300

    ready for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: build/install/work.racka.thinkrchive-server/bin/work.racka.thinkrchive-server
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+val hikari_version: String by project
+val postgres_version: String by project
+val koin_version: String by project
 
 plugins {
     application
@@ -12,6 +16,10 @@ group = "work.racka"
 version = "0.0.1"
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+tasks.create("stage") {
+    dependsOn("installDist")
 }
 
 repositories {
@@ -28,4 +36,17 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    // Exposed
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    // Postgres
+    implementation("org.postgresql:postgresql:$postgres_version")
+    // Hikari
+    implementation("com.zaxxer:HikariCP:$hikari_version")
+    // Koin core features
+    implementation("io.insert-koin:koin-core:$koin_version")
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
 }
